585ccdccab8c90adc1e085ffa72c4f4283032081
Rich Churcher <rich.churcher@gmail.com>
2020-03-28 10:13:41 +1300

    Ch 5: field of view

--- src/components.rs
+++ src/components.rs
@@ -1,5 +1,6 @@
 use rltk::RGB;
 use specs::prelude::*;
+use specs_derive::*;
 
 #[derive(Component)]
 pub struct Position {
@@ -16,3 +17,10 @@ pub struct Renderable {
 
 #[derive(Component, Debug)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Viewshed {
+    pub visible_tiles: Vec<rltk::Point>,
+    pub range: i32,
+    pub dirty: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use rltk::{Console, GameState, Rltk, RGB};
 use specs::prelude::*;
-#[macro_use]
-extern crate specs_derive;
 mod components;
 pub use components::*;
 mod map;
@@ -10,6 +8,8 @@ mod player;
 use player::*;
 mod rect;
 pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 pub struct State {
     pub ecs: World,
@@ -17,6 +17,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -28,8 +30,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -49,10 +50,11 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
@@ -66,6 +68,11 @@ fn main() {
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+            dirty: true,
+        })
         .build();
 
     rltk::main_loop(context, gs);
